e0c87af8ac70d039e8e5ee0787a86256f30c6ca9
eddie-code <231071372@qq.com>
2025-05-14 21:09:15 +0800

    feat(audio): 9.7 添加音频文件选择功能
    - 在 a-modal 标签中添加了选择音频文件的按钮
    - 实现了文件选择逻辑和按钮点击事件处理

--- plus-ui-ts/src/views/audio/voiceRecognition/filetrans-upload.vue
+++ plus-ui-ts/src/views/audio/voiceRecognition/filetrans-upload.vue
@@ -1,5 +1,9 @@
 <template>
   <a-modal v-model:open="open" title="Basic Modal" @ok="handleOk">
+    <a-button type="primary" size="large" @click="handleClick">
+      <span><UploadOutlined /> 选择者音频</span>
+    </a-button>
+    <input type="file" style="display: none" ref="fileUploadCom"/>
     <p>Some contents...</p>
     <p>Some contents...</p>
     <p>Some contents...</p>
@@ -18,6 +22,19 @@ const showModal = () => {
 const handleOk = (e) => {
   console.log(e);
   open.value = false;
+};
+
+// ----------- 选择文件 -----------
+const fileUploadCom = ref();
+const selectFile = () => {
+  console.log('此处为文件选择逻辑');
+  fileUploadCom.value.click();
+};
+
+// 处理按钮点击事件
+const handleClick = () => {
+  showModal();
+  selectFile();
 };
 
 // 使用 defineExpose 向外部暴露指定的数据和方法
