6a3be926df465b79fb32442ed462c0256b3b8fed
eddie-code <231071372@qq.com>
2025-05-14 21:35:10 +0800

    feat(audio): 9.7 限制上传文件的大小
    - 实现文件大小限制校验
    - 使用 Ant Design Vue 的 notification 组件显示提示信息
    - 优化文件选择和上传流程

--- plus-ui-ts/src/views/audio/voiceRecognition/filetrans-upload.vue
+++ plus-ui-ts/src/views/audio/voiceRecognition/filetrans-upload.vue
@@ -3,7 +3,12 @@
     <a-button type="primary" size="large" @click="handleClick">
       <span><UploadOutlined /> 选择者音频</span>
     </a-button>
-    <input type="file" style="display: none" ref="fileUploadCom" accept=".mp3,.wav,.m4a" />
+    <input type="file"
+           style="display: none"
+           ref="fileUploadCom"
+           accept=".mp3,.wav,.m4a"
+           @change="uploadFile"
+    />
     <p>Some contents...</p>
     <p>Some contents...</p>
     <p>Some contents...</p>
@@ -12,6 +17,7 @@
 
 <script setup>
 import { ref } from 'vue';
+import { notification } from 'ant-design-vue';
 
 const open = ref(false);
 
@@ -27,8 +33,30 @@ const handleOk = (e) => {
 // ----------- 选择文件 -----------
 const fileUploadCom = ref();
 const selectFile = () => {
-  console.log('此处为文件选择逻辑');
+  console.log('选择文件逻辑');
+  fileUploadCom.value.value = "";
   fileUploadCom.value.click();
+};
+
+// ----------- 上传文件 -----------
+const uploadFile = () => {
+  console.log('上传文件逻辑');
+  const file = fileUploadCom.value.files[0];
+  console.log(file)
+
+  // 音频的文件最大为500M
+  let max = 500 * 1024 * 1024;
+  // let max = 1 * 1024; // 测试用
+  let size = file.size;
+  if (size > max){
+    // [Notification 通知提醒框] https://2x.antdv.com/components/notification-cn
+    notification['warning']({
+      message: '系统提示',
+      description: '文件大小超过最大限制, 最大为500M'
+    });
+    return;
+  }
+
 };
 
 // 处理按钮点击事件
